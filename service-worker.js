a4f4eeab32ce734c6086cfa1f9b6a3643dd23370
Shuxrat <shuukhratbegmatov@gmail.com>
2025-01-26 00:48:08 +0500

    fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff4e4570a6f407d3b36065624f75ba6b.js"
+  "/precache-manifest.3d9b13d6a6cd0f74cedf8462a968c4c5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kasbmaktab.uz"});
